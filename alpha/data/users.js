f09d7e9683b9604e2a081d1bccd3ccebcb09f3bc
cindytran211 <72306508+cindytran211@users.noreply.github.com>
2022-04-21 11:51:31 -0400

    Update users.js

--- alpha/data/users.js
+++ alpha/data/users.js
@@ -1,277 +1,143 @@
-const { ObjectId } = require("mongodb");
-const mongoCollections = require("../config/mongoCollections");
-const usersCol = mongoCollections.users;
+const mongoCollections= require('../config/mongoCollections');
+const users=mongoCollections.users;
+const { ObjectId }=require('mongodb');
+const bcrypt=require('bcrypt');
+const validation = require('../validation');
 
-const bcrypt = require("bcryptjs");
-
-const salt = 16;
-
-const debug = true;
-const logDebug = function logDebug(str) {
-  if (debug) console.error(str);
+let exportedMethos=
+{
+    async createUser(firstname,lastname,email,age,street,city,state,zipcode,phonenumber,username,password)
+    {
+        firstname=validation.checkFirstName(firstname);
+        lastname=validation.checkLastName(lastname);
+        age=validation.checkAge(age);
+        street=validation.checkStreet(street);
+        city=validation.checkCity(city);
+        state=validation.checkState(state);
+        zipcode=validation.checkZipcode(zipcode);
+        phonenumber=validation.checkPhoneNumber(phonenumber);
+        username=validation.checkUserName(username);
+        password=validation.checkPassWord(password);
+        email=validation.checkEmail(email);
+        //const newUserName=username.toLowerCase();
+        const usersCollection= await users();
+        const findUser=await usersCollection.find({username: username}).toArray();
+        if(findUser.length==0)
+        {
+            const saltRounds=16;
+            const hash=await bcrypt.hash(password,saltRounds);
+            const holder=
+            {
+                username: username,
+                password: hash,
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                age: age,
+                street: street,
+                city: city,
+                state: state,
+                zipcode: zipcode,
+                mobilephone: phonenumber,
+            }
+            const newUser=await usersCollection.insertOne(holder);
+            if(!newUser.acknowledged || !newUser.insertedId)
+            {
+                throw "Error: Could not Add User"
+            }
+            const inserted=
+            {
+                userInserted: true
+            }
+            return inserted;
+        }
+        else
+        {
+            throw "Error: There is Already A User With That UserName"
+        }
+    },
+    async checkUser(username,password)
+    {
+        username=validation.checkUserName(username);
+        password=validation.checkPassWord(password);
+        //const newUserName=username.toLowerCase();
+        const usersCollection= await users();
+        const findUser=await usersCollection.find({username: username}).toArray();
+        if(!findUser)
+        {
+            throw "Error: Either the username or password is invalid"
+        }
+        else
+        {
+            const comparePasswords=await bcrypt.compare(password,findUser[0].password);
+            if(comparePasswords)
+            {
+                const holder=
+                {
+                    authenticated: true
+                }
+                return holder;
+            }
+            else
+            {
+                throw "Error: Either the username or password is invalid"
+            }
+        }
+    },
+    async deleteUser(username)
+    {
+        username=validation.checkUserName(username);
+        const usersCollection= await users();
+        const deletionInfo=await usersCollection.deleteOne({username: username});
+        if(deletionInfo.deletedCount===0)
+        {
+            throw 'Error: Could not delete user';
+        }
+        return username;
+    },
+    async findUser(username)
+    {
+        username=validation.checkUserName(username);
+        const usersCollection= await users();
+        const findUser=await usersCollection.find({username: username}).toArray();
+        if(!findUser)
+        {
+            throw "Error: Cannot find user"
+        }
+        else
+        {
+            return findUser;
+        }
+    },
+    async updateUser(firstname,lastname,email,age,street,city,state,zipcode,phonenumber,username,password)
+    {
+        const saltRounds=16;
+        const hash=await bcrypt.hash(password,saltRounds);
+        const newUserInfo=
+        {
+            username: username,
+            password: hash,
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            age: age,
+            street: street,
+            city: city,
+            state: state,
+            zipcode: zipcode,
+            mobilephone: phonenumber,
+        }
+        const oldUser=await this.findUser(username);
+        const userCollection=await users();
+        const updateInfo=await userCollection.updateOne(
+            {_id:oldUser[0]._id},{$set:newUserInfo}
+        );
+        if(!updateInfo.matchedCount && !updateInfo.modifiedCount)
+        {
+            throw "Error: Update failed";
+        }
+        return newUserInfo;
+    }
 };
 
-
-function logit(str) {
-  console.log("[" + new Date().toUTCString() + "]: " + str);
-}
-
-async function checkuserId(userId) {
-
-  let found = false;
-  userId = userId.toLowerCase();
-
-  if (userId.search(/^[a-z0-9]{4,}$/) < 0)
-    throw "userId Illegal chars or Not long enough";
-
-  const usersCollection = await usersCol();
-  let user = await usersCollection
-    .find({}, { projection: { _id: 1, userId: 1 } })
-    .toArray();
-  user.forEach((element) => {
-    if (element.userId == userId) found = true;
-  });
-
-  return found;
-}
-
-async function createUser(userId, passWord) {
-  userId = userId.toLowerCase();
-
-  if (userId.search(/^[a-z0-9]{4,}$/) < 0)
-    throw "userId Illegal chars or Not long enough";
-
-  if (userId.search(/\s/) >= 0) throw "Password has spaces";
-
-  if (passWord.length < 6) throw "Password not long enough";
-
-  if (checkuserId(userId) == true) throw "Name in Use Already";
-
-  let hashPass = await bcrypt.hash(passWord, salt);
-
-  const newUserDb = {
-    userId: userId,
-    password: hashPass,
-    paymentArray: [],
-    orderArray: [],
-    favorites: []
-  };
-
-
-  const userCollection = await usersCol();
-  const insertInfo = await userCollection.insertOne(newUserDb);
-  if (insertInfo.insertedCount === 0) {
-    throw "Failed to create " + newUserDb;
-  }
-
-  logDebug(" user created is true " + userId);
-
-  return { userInserted: true };
-}
-
-async function checkUser(userId, passWord) {
-  let userMatch = {};
-  let found = false;
-  userId = userId.toLowerCase();
-
-  logDebug("checkUser "  + userId);
-
-  if (userId.search(/^[a-z0-9]{4,}$/) < 0)
-    throw "userId Illegal chars or Not long enough";
-
-  const usersCollection = await usersCol();
-  let user = await usersCollection.find({}, { projection: { _id: 1, userId: 1 , password: 1} }).toArray();
-  user.forEach((element) => {
-    if (element.userId == userId) { 
-      userMatch.userId = element.userId;
-      userMatch.password = element.password;
-      found = true;
-    }
-  });
-
-  if (found == false) throw "Can not find user";
-
-  let compareToMatch = false;
-  logDebug("Check pass " + passWord);
-  logDebug(userMatch);
-
-  let itMatches = false;
-  try {
-    logDebug(" check passwd "+ passWord + " ", userMatch.password);
-    itMatches = await bcrypt.compare(passWord, userMatch.password);
-    if (itMatches) logDebug("They match ");
-    else {
-      logDebug( " failed match " + passWord + " to " + userMatch.clear + " " + userMatch.password );
-      throw "Failed password match"
-    }
-  } catch (e) {
-    logDebug( "Error Check user ...  (Non-Authenticated User)" );
-    throw "Either the userId or password is invalid";
-  }
-
-  return { authenticated: true };
-}
-
-async function getUser(userId) {
-  let userMatch = {};
-  let found = false;
-  userId = userId.toLowerCase();
-
-  logDebug("checkUser");
-
-  if (userId.search(/^[a-z0-9]{4,}$/) < 0)
-    throw "userId Illegal chars or Not long enough";
-
-
-  const usersCollection = await usersCol();
-  let query = { userId: userId };
-  logDebug( query );
-  let proj = { 
-      _id: 1, 
-      userId: 1, 
-      firstName: 1,
-      lastName: 1,
-      email: 1  ,
-      age: 1,
-      mobilePhone: 1  ,
-      streetAddress: 1  ,
-      city: 1 ,
-      state: 1  ,
-      zipcode: 1  
-      }
-
-  let user = await usersCollection.find( query, { projection: proj  }).toArray();
-  user.forEach((element) => {
-    if (element.userId == userId) { 
-      userMatch.userId = element.userId;
-      userMatch.firstName  = element.firstName ;
-      userMatch.lastName  = element.lastName ;
-      userMatch.email  = element.email ;
-      userMatch.mobilePhone  = element.mobilePhone ;
-      userMatch.streetAddress  = element.streetAddress ;
-      userMatch.city  = element.city ;
-      userMatch.state  = element.state ;
-      userMatch.zipcode  = element.zipcode ;
-      userMatch.age  = element.age ;
-      
-      found = true;
-      logDebug(element);
-    }
-  });
-
-  if (found == false) throw "Can not find user";
-
-  let compareToMatch = false;
-  logDebug(userMatch);
-
-  let rtn = {
-    userId: userMatch.userId,
-    firstName: userMatch.firstName  ,
-    lastName:userMatch.lastName  ,
-    email:userMatch.email  ,
-    mobilePhone:userMatch.mobilePhone  ,
-    streetAddress:userMatch.streetAddress  ,
-    city:userMatch.city  ,
-    state:userMatch.state  ,
-    zipcode:userMatch.zipcode  ,
-    age:userMatch.age,
-    authenticated: true
-    }
-
-  return rtn ;
-}
-
-async function createUser(userId, passWord) {
-  userId = userId.toLowerCase();
-
-  if (userId.search(/^[a-z0-9]{4,}$/) < 0)
-    throw "userId Illegal chars or Not long enough";
-
-  if (userId.search(/\s/) >= 0) throw "Password has spaces";
-
-  if (passWord.length < 6) throw "Password not long enough";
-
-  if (checkuserId(userId) == true) throw "Name in Use Already";
-
-  let hashPass = await bcrypt.hash(passWord, salt);
-
-  const newUserDb = {
-    userId: userId,
-    password: hashPass
-  };
-
-
-  const userCollection = await usersCol();
-  const insertInfo = await userCollection.insertOne(newUserDb);
-  if (insertInfo.insertedCount === 0) {
-    throw "Failed to create " + newUserDb;
-  }
-
-  logDebug(" user created is true " + userId);
-
-  return { userInserted: true };
-}
-
-async function setUser( up ) {
-  let userMatch = {};
-  let found = false;
-  let userId = up.userId.toLowerCase();
-
-  logDebug("checkUser");
-
-  if (userId.search(/^[a-z0-9]{4,}$/) < 0)
-    throw "userId Illegal chars or Not long enough";
-
-    const newUser = {
-      firstName: up.firstName,
-      lastName : up.lastName,
-      email : up.email,
-      mobilePhone : up.mobilePhone,
-      streetAddress : up.streetAddress,
-      city: up.city,
-      state: up.state,
-      zipcode: up.zipcode,
-      age: up.age
-    };
-
-  const usersCollection = await usersCol();
-  let user = await usersCollection.find({}, { projection: { _id: 1, userId: 1 , firstName:1, lastName: 1 } }).toArray();
-  user.forEach((element) => {
-    if (element.userId == userId) { 
-      userMatch.userId = element.userId;
-      //userMatch.password = element.password;
-      userMatch._id = element._id;
-      found = true;
-    }
-  });
-
-  let updateInfo;
-  try {
-    updateInfo = await usersCollection.updateOne( { _id: userMatch._id },{ $set:  newUser } );
-    logDebug(updateInfo);
-    } catch (e) {
-    // if (updateInfo.insertedCount === 0) {
-        logDebug("updateone failed");
-        logDebug(updateInfo);
-        throw "Failed to update "+ userId;
-    }
-
-  if (found == false) throw "Can not find user";
-
-  let compareToMatch = false;
-  logDebug(userMatch);
-
-  let itMatches = false;
-  return { authenticated: true };
-}
-
-
-
-
-
-module.exports = {
-  checkUser,
-  createUser,
-  getUser,
-  setUser
-};
+module.exports=exportedMethos;
