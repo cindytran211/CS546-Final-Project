1f94ec25fb18cda241908dad5de72a0e85281946
cindytran211 <Cindy795Tran@gmail.com>
2022-04-21 14:00:45 -0400

    tidying up code

--- alpha/routes/index.js
+++ alpha/routes/index.js
@@ -1,12 +1,13 @@
-const allRoutes=require('./allRoutes');
-
+const userRoutes=require('./userRoutes');
+const paymentRoutes=require('./addPayment');
 const constructorMethod=(app) => {
     //code to set up the site. All urls get passed to petRoutes. 
     //Extranoues urls would just get redirected to the homepage
-   app.use('/',allRoutes);
+   app.use('/',userRoutes);
    app.use('*', (req,res) => {
        res.redirect('/');
    });
+   app.use('/addPayment',paymentRoutes);
 };
 
 module.exports=constructorMethod;
